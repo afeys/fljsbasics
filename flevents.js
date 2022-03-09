61d0e8d25ae10541736bd111ee294a79e892bc3d
afeys <andy@feys.be>
2022-03-09 18:12:42 +0100

    added extra option to flevents: setSender, for if you already have the element in a variable.

--- flevents.js
+++ flevents.js
@@ -16,9 +16,11 @@
 //      console.log("function handleprodtypechange called " + idx);
 //   }
 //   function initializeEvents() {
+//      let testelement = document.getElementById("myId");
 //      const eventhandler = new FLEventHandler();
 //      eventhandler.addEvent().setEventType("change").setEventSenderId("fldtypeid").addEventReceiver("handletypechangesubtype").addEventReceiver("handletypechangename");
 //      eventhandler.addEvent().setEventType("change").setEventSenderName("fldprodtype[]").addEventReceiver("handleprodtypechange");
+//      eventhandler.addEvent().setEventType("click").setEventSender(testelement).addEventReceiver("handleprodtypechange");
 //      eventhandler.activate();
 //   }
 //   FLFunctions.pageLoaded(initializeEvents());
@@ -36,6 +38,7 @@
 
 class FLEvent {
     _type = "change";
+    _sender = "";           // set the sender by element
     _sendername = "";       // you can set the sender either by name (for formfields for example)
     _senderid = "";         // or by id, for html elements that don't have a name property, or by blockid. Blockid is used to 
     // for mutationobserver. (typically this will be used to monitor changes anywhere in a div, article or span). Set Blockid to the id of the div, or whatever you want
@@ -49,18 +52,28 @@ class FLEvent {
     }
     setEventSenderName(name) {
         this._sendername = name;
+        this._sender = "";
         this._senderid = "";        // if name is set, then id can't be set.
         this._senderblockid = "";   // if name is set, then blockid can't be set.
         return this;
     }
+    setEventSender(el) {
+        this._sender = el;
+        this._senderid = "";
+        this._sendername = "";      // if id is set, then name can't be set.
+        this._senderblockid = "";   // if id is set, then blockid can't be set.
+        return this;
+    }
     setEventSenderId(id) {
         this._senderid = id;
+        this._sender = "";
         this._sendername = "";      // if id is set, then name can't be set.
         this._senderblockid = "";   // if id is set, then blockid can't be set.
         return this;
     }
     setEventSenderBlockId(id) {
         this._senderblockid = id;
+        this._sender = "";
         this._senderid = "";        // if blockid is set, then id can't be set.
         this._sendername = "";      // if blockid is set, then name can't be set.
         return this;
@@ -94,6 +107,25 @@ class FLEvent {
             }
         }
     }
+    attachEventListenerByElement() {
+        let senderel = this._sender;
+        for (let receiver of this._receivers) {
+                if (this._type === "enterkey") {
+                    for (let receiver of this._receivers) {
+                        senderel.addEventListener("keyup", function (e) {
+                            e.preventDefault();
+                            if (e.keyCode === 13) {
+                                window[receiver](e);
+                            }
+                        });
+                    }
+                } else {
+                    senderel.addEventListener(this._type, function (e) {
+                        window[receiver](e);
+                    });
+                }
+        }
+    }
     attachEventListenerById() {
         let senderel = document.getElementById(this._senderid);
         for (let receiver of this._receivers) {
@@ -119,6 +151,10 @@ class FLEvent {
         } else {
             if (this._senderid !== "") {
                 this.attachEventListenerById();
+            } else {
+                if (this._sender !== "") {
+                    this.attachEventListenerByElement();
+                }
             }
         }
     }
